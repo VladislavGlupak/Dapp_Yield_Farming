598fd15d28bb93dbe45af159ead6c877acd57fb8
Vladislav Glupak <vglupak@gmail.com>
2022-01-13 13:56:21 -0800

    adding Transfer function and event to Tether sol

--- src/contracts/Tether.sol
+++ src/contracts/Tether.sol
@@ -6,5 +6,32 @@ contract Tether {
     uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens
     uint public decimals = 18;
 
+    event Tranfer(
+        address indexed _from,
+        address indexed _to,
+        uint _value
+    );
 
+    event Approve(
+        address indexed _owner,
+        address indexed _spender,
+        uint _value
+    );
+
+    mapping(address => uint256) public balanceOf;
+
+    constructor() {
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint _value) public returns (bool success) {
+        require(balanceOf[msg.sender] => _value); // require the value greter or equal for transfer
+
+        balanceOf[msg.sender] -= _value; // transfer the amount and subtract the balance
+
+        balanceOf[_to] += _value; // add the balance
+
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
 }
