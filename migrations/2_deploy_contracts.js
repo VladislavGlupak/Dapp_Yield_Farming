85895269df16956aa7fe541c8399fa68c72f3ecc
Vladislav Glupak <vglupak@gmail.com>
2022-01-13 16:22:39 -0800

    adding RWD, Decentral banl contracts, change deploy contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,23 @@
 const Tether = artifacts.require("Tether");
+const RWD = artifacts.require("RWD");
+const DecentralBank = artifacts.require("DecentralBank");
 
-module.exports = async function (deployer){
+module.exports = async function (deployer, network, accounts){
+    // deploy Tether contract
     await deployer.deploy(Tether)
-};
+    const tether = await Tether.deployed()
+
+    // deploy RWD (reward) contract
+    await deployer.deploy(RWD)
+    const rwd = await RWD.deployed()
+
+    // deploy DecentralBank contract
+    await deployer.deploy(DecentralBank, rwd.address, tether.address) 
+    const decentralBank = await DecentralBank.deployed()
+
+    // transfer all rewards to Decentral bank
+    await rwd.transfer(decentralBank.address, '1000000000000000000000000')
+
+    // distribute 100 Tether tokens to investor
+    await tether.transfer(accounts[1], '100000000000000000000')
+}
--- src/contracts/RWD.sol
+++ src/contracts/RWD.sol
@@ -1,8 +1,10 @@
+// Reward token
+
 pragma solidity ^0.5.0;
 
-contract Tether {
-    string public name = "Tether";
-    string public symbol = "USDT";
+contract RWD {
+    string public name = "Reward Token";
+    string public symbol = "RWD";
     uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens
     uint public decimals = 18;
 
