68a37c77971171c66e2777fc1abc634c785a0292
Vladislav Glupak <vglupak@gmail.com>
2022-01-26 15:41:50 -0800

    adding navbar and app

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react'
+import './App.css'
+import Navbar from './Navbar'
+
+class App extends Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            account: '0x0' // initialize state
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <Navbar account={this.state.account}/>
+                <div className='text-center'>
+                    <h></h>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App;
