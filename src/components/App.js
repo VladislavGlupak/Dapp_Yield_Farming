cabfaeba63d689750ffda3eab50bcec1aa7fff67
Vladislav Glupak <vglupak@gmail.com>
2022-01-27 16:04:34 -0800

    loading blocjchain data

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,81 @@
 import React, {Component} from 'react'
 import './App.css'
-import Navbar from './Navbar'
+import Navbar from './Navbar';
+import Web3 from 'web3';
+import Tether from '../truffle_abis/Tether.json'
+import RWD from '../truffle_abis/RWD.json'
+import DecentralBank from '../truffle_abis/DecentralBank.json'
 
 class App extends Component {
+
+    async UNSAFE_componentWillMount() {
+        await this.loadWeb3()
+        await this.loadBlockchainData()
+    }
+
+    async loadWeb3() {
+        if(window.ethereum) {
+            window.web3 = new Web3(window.ethereum)
+            await window.ethereum.enable()
+        } else if (window.web3) {
+            window.web3 = new Web3(window.web3.currentProvider)
+        } else {
+            window.alert('No ethereum browser detected! Check out Metamask!')
+        }
+    }
+
+    async loadBlockchainData() {
+        const web3 = window.web3
+        const account = await web3.eth.getAccounts()
+        this.setState({account: account[0]})
+        const networkId = await web3.eth.net.getId()
+
+        // load Tether contract
+        const tetherData = Tether.networks[networkId]
+        if(tetherData) {
+            const tether = new web3.eth.Contract(Tether.abi, tetherData.address)
+            this.setState({tether})
+            let tetherBalance = await tether.methods.balanceOf(this.state.account).call()
+            this.setState({tetherBalance: tetherBalance.toString()})
+        } else {
+            window.alert('Error! Tether contract not deployed - no detected network!')
+        }
+
+        // load Reward contract
+        const rwdData = RWD.networks[networkId]
+        if(rwdData) {
+            const rwd = new web3.eth.Contract(RWD.abi, tetherData.address)
+            this.setState({rwd})
+            let rwdBalance = await rwd.methods.balanceOf(this.state.account).call()
+            this.setState({rwdBalance: rwdBalance.toString()})
+        } else {
+            window.alert('Error! RWD contract not deployed - no detected network!')
+        }
+
+        // load DecentralBank contract
+        const decentralBank = DecentralBank.networks[networkId]
+        if(decentralBank) {
+            const decentralBank = new web3.eth.Contract(DecentralBank.abi, decentralBank.address)
+            this.setState({decentralBank})
+            let stakingBalance = await decentralBank.methods.stakingBalance(this.state.account).call()
+            this.setState({stakingBalance: stakingBalance.toString()})
+        } else {
+            window.alert('Error! Decentral bank contract not deployed - no detected network!')
+        }
+        this.setState({loading: false})
+    }
 
     constructor(props){
         super(props)
         this.state = {
-            account: '0x0' // initialize state
+            account: '0x0', // initialize state
+            tether: {},
+            rwd: {},
+            decentralBank: {},
+            tetherBalance: '0',
+            rwdBalance: '0',
+            stakingBalance: '0',
+            loading: true
         }
     }
 
@@ -16,7 +84,9 @@ class App extends Component {
             <div>
                 <Navbar account={this.state.account}/>
                 <div className='text-center'>
-                    <h></h>
+                    <h1>
+                        {console.log(this.state.loading)}
+                    </h1>
                 </div>
             </div>
         )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ class Navbar extends Component {
             <nav className='navbar navbar-dark fixed-top shadow p-0' style={{backgroundColor: 'black', height: '50px'}}>
                 <a className='navbar-brand col-sm-3 col-md-2 mr-0'
                 style={{color: 'white'}}>
-                <img src={bank} width='50' height='30' className='d-inline-block align-top' alt='bank image'/>
+                <img src={bank} width='50' height='30' className='d-inline-block align-top' alt='bank'/>
                 &nbsp;&nbsp; Dapp Yield Staking (Decentralized Banking)
                 </a>
                 <ul className='navbar-nav px-3'>
